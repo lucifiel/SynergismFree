8d93bab89d151404f7d303d09c200d015240f61a
lucifiel <1196947+lucifiel@users.noreply.github.com>
2021-07-04 19:51:37 +0300

    Add clipboard copy fallback

--- src/ImportExport.ts
+++ src/ImportExport.ts
@@ -64,6 +64,24 @@ const saveFilename = () => {
     return t;
 }
 
+const legacyCopyToClipboard = (save?: string) => {
+    // Old browsers (legacy Edge, Safari 13.0)
+    const textArea = document.createElement('textarea');
+    textArea.value = save;
+    textArea.setAttribute('style', 'top: 0; left: 0; position: fixed;');
+
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+    try {
+        document.execCommand('copy');
+    } catch (_) {
+        console.error("Failed to copy savegame to clipboard.");
+    }
+
+    document.body.removeChild(textArea);
+}
+
 export const exportSynergism = async () => {
     player.offlinetick = Date.now();
     const quarkData = quarkHandler();
@@ -78,23 +96,12 @@ export const exportSynergism = async () => {
     const save = localStorage.getItem('Synergysave2');
     if ('clipboard' in navigator && toClipboard) {
         await navigator.clipboard.writeText(save)
-            .catch(e => console.error(e));
+            .catch(e => {
+                console.error(e)
+                legacyCopyToClipboard(save)
+            })
     } else if (toClipboard) {
-        // Old browsers (legacy Edge, Safari 13.0)
-        const textArea = document.createElement('textarea');
-        textArea.value = save;
-        textArea.setAttribute('style', 'top: 0; left: 0; position: fixed;');
-
-        document.body.appendChild(textArea);
-        textArea.focus();
-        textArea.select();
-        try {
-            document.execCommand('copy');
-        } catch (_) {
-            console.error("Failed to copy savegame to clipboard.");
-        }
-
-        document.body.removeChild(textArea);
+        legacyCopyToClipboard(save);
     } else {
         const a = document.createElement('a');
         a.setAttribute('href', 'data:text/plain;charset=utf-8,' + save);
