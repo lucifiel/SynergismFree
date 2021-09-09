e98ce29cdd20c6a5ac2f4aaf8abe77d5045e3965
lucifiel <1196947+lucifiel@users.noreply.github.com>
2021-09-09 23:14:41 +0300

    Hide hours when you're over 100 days

--- src/Synergism.ts
+++ src/Synergism.ts
@@ -1588,11 +1588,19 @@ export const format = (
 }
 
 export const formatTimeShort = (seconds: number, msMaxSeconds?: number): string => {
-    return ((seconds >= 86400) ? format(Math.floor(seconds / 86400)) + "d" : '') +
-        ((seconds >= 3600) ? format(Math.floor(seconds / 3600) % 24) + "h" : '') +
-        ((seconds >= 60)   ? format(Math.floor(seconds / 60) % 60) + "m"   : '') +
-        ((seconds >= 8640000) ? '' : format(Math.floor(seconds) % 60) + ((msMaxSeconds && seconds < msMaxSeconds)  //Don't show seconds when you're over 100 days, like honestly
-            ? "." + (Math.floor((seconds % 1) * 1000).toString().padStart(3, '0')) : '') + "s");
+    const d = ((seconds >= 86400) ? format(Math.floor(seconds / 86400)) + "d" : '');
+    const h = ((seconds >= 3600) ? format(Math.floor(seconds / 3600) % 24) + "h" : '');
+    const m = ((seconds >= 60) ? format(Math.floor(seconds / 60) % 60) + "m" : '');
+    const ss = ((msMaxSeconds && seconds < msMaxSeconds)
+        ? "." + (Math.floor((seconds % 1) * 1000).toString().padStart(3, '0')) : '');
+    //Don't show seconds when you're over 100 days, like honestly
+    const s = ((seconds >= 8640000) ? '' : format(Math.floor(seconds) % 60) + ss + "s");
+
+    if (seconds >= 86400000) {
+        // Hide hours when you're over 1000 days
+        return d;
+    }
+    return d + h + m + s;
 }
 
 export const updateAllTick = (): void => {
