3f86d99fa41b16e0d22c90428a4407f90b3bb2b1
lucifiel <lucifiel@users.noreply.github.com>
2024-07-02 06:52:22 +0300

    !

--- src/Config.ts
+++ src/Config.ts
@@ -10,7 +10,7 @@ export const version = '3.0.0 pt 5.2: June 27, 2024: Fall of the Octeracts 2'
  */
 export const testing: boolean = false
 export const lastUpdated0 = new Date('##LAST_UPDATED##')
-export const lastUpdated = new Date(2024, 5, 20)
+export const lastUpdated = new Date(2024, 5, 21)
 /**
  * CHANGE THIS ONE INSTEAD
  */
