9aa2c4a615150bdc6f922548d77abf306de9eb5b
lucifiel <lucifiel@users.noreply.github.com>
2024-07-07 06:28:57 +0300

    Set release date

--- src/Config.ts
+++ src/Config.ts
@@ -9,7 +9,8 @@ export const version = '3.0.0 pt 5.2: June 27, 2024: Fall of the Octeracts 2'
  * PSEUDO DO NOT CHANGE THIS LINE
  */
 export const testing: boolean = false
-export const lastUpdated = new Date('##LAST_UPDATED##')
+export const lastUpdated0 = new Date('##LAST_UPDATED##')
+export const lastUpdated = new Date(2024, 5, 25)
 /**
  * CHANGE THIS ONE INSTEAD
  */
